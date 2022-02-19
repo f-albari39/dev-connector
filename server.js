51abd37564538690f2cb6b63bf6931274e717d56
f-albari39 <fakhrialbari.id@gmail.com>
2022-02-20 05:53:41 +0700

    Eps 8 - Route Files With Express Router

--- server.js
+++ server.js
@@ -15,3 +15,9 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.send("Api running")
 })
+
+// define route
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
