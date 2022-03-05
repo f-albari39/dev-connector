bb37fc814da2af5c3585368c6785b94b92bc695f
f-albari39 <fakhrialbari.id@gmail.com>
2022-03-05 20:35:00 +0700

    Eps 25 - Add Post Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,44 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   GET api/users
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => {
-  res.send("post route");
-});
+const auth = require("../../middleware/auth");
+const Post = require("../../models/Post");
+const User = require("../../models/User");
+
+const { check, validationResult } = require("express-validator");
+
+// @route   POST api/posts
+// @desc    Post a post
+// @access  Private
+
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ msg: "Server Error" });
+    }
+  }
+);
 
 module.exports = router;
