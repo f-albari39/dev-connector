a4a4a9b6dba75f128a369c1b214ae2ff643ffcf9
f-albari39 <fakhrialbari.id@gmail.com>
2022-03-17 23:22:27 +0700

    Eps 35 - Request Example and Login Form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <>Login</>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="container">
+      <h1 className="large text-primary">Login</h1>
+      <form className="form" action="create-profile.html" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            onChange={onChange}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Create new account <Link to="/register">Register</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
